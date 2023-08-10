6908b4d6116758088179e675a5f00398a87b6fe2
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 08:42:14 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { startWith } from 'rxjs/operators';
 import { of } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/startwith
+
 //emit (1,2,3)
 const src = of(1, 2, 3);
 const example = src.pipe(startWith('first val'));
